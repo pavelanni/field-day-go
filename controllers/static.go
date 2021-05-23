bd007e59051644195fd4a2ee8f6aa9fb00a8fd5a
Pavel Anni <pavel.anni@gmail.com>
2021-05-23 12:28:02 -0400

    Clean up to be close to the book

--- controllers/static.go
+++ controllers/static.go
@@ -4,14 +4,12 @@ import "field-day/views"
 
 func NewStatic() *Static {
 	return &Static{
-		Home:     views.NewView("bootstrap", "static/home"),
-		Contact:  views.NewView("bootstrap", "static/contact"),
-		Notfound: views.NewView("bootstrap", "static/notfound"),
+		Home:    views.NewView("bootstrap", "static/home"),
+		Contact: views.NewView("bootstrap", "static/contact"),
 	}
 }
 
 type Static struct {
-	Home     *views.View
-	Contact  *views.View
-	Notfound *views.View
+	Home    *views.View
+	Contact *views.View
 }
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"field-day/controllers"
-	"field-day/views"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -15,55 +14,22 @@ type Visitor struct {
 	Nfarl     bool
 }
 
-var (
-	homeView     *views.View
-	contactView  *views.View
-	listView     *views.View
-	notfoundView *views.View
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "mysecretpassword"
+	dbname   = "fieldday_dev"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(notfoundView.Render(w, nil))
-}
-
-func list(w http.ResponseWriter, r *http.Request) {
-	data := []Visitor{{"Pavel Anni", "AC4PA", false, true},
-		{"Jim Stafford", "W4QO", false, true},
-		{"Gene Shablygin", "W3UA", true, false}}
-
-	w.Header().Set("Content-Type", "text/html")
-	must(listView.Render(w, data))
-}
-
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
 
 func main() {
 	staticC := controllers.NewStatic()
-	notfoundView = views.NewView("bootstrap", "static/notfound")
-	listView = views.NewView("bootstrap", "list")
 	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/list", list).Methods("GET")
 	r.Handle("/", staticC.Home).Methods("GET")
 	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/new", usersC.New).Methods("GET")
 	r.HandleFunc("/new", usersC.Create).Methods("POST")
-	r.NotFoundHandler = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", r)
 }
