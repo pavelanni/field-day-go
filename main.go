4c6a4b95f4e8eb9ceb7016f6a7178ce11f8c44ca
Pavel Anni <pavel.anni@gmail.com>
2021-07-10 21:54:27 -0400

    Remove gorilla/mux, use net/http

--- main.go
+++ main.go
@@ -1,45 +1,112 @@
 package main
 
 import (
-	"field-day/controllers"
-	"field-day/models"
+	"fmt"
+	"html/template"
+	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
+	"reflect"
 )
 
 type Visitor struct {
-	Name      string
-	Callsign  string
-	FirstTime bool
-	Nfarl     bool
+	FirstName string `form:"firstname"`
+	LastName  string `form:"lastname"`
+	Callsign  string `form:"callsign"`
+	Email     string `form:"email"`
+	Nfarl     bool   `form:"nfarl"`
+	Contactme bool   `form:"contactme"`
+	Youth     bool   `form:"youth"`
+	Firsttime bool   `form:"firsttime"`
 }
 
-const (
-	dbfile = "field-day.db"
-)
+var templateDir = "templates"
+var staticDir = "static"
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		templateDir + "/bootstrap-refresh.go.html",
+		templateDir + "/header.go.html",
+		templateDir + "/home.go.html",
+		templateDir + "/footer.go.html",
+	}
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tmpl.Execute(w, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func confirmHandler(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		templateDir + "/bootstrap-refresh.go.html",
+		templateDir + "/header.go.html",
+		templateDir + "/confirmation.go.html",
+		templateDir + "/footer.go.html",
+	}
+	tmpl, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tmpl.Execute(w, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func newHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		files := []string{
+			templateDir + "/bootstrap.go.html",
+			templateDir + "/header.go.html",
+			templateDir + "/new.go.html",
+			templateDir + "/footer.go.html",
+		}
+		tmpl, err := template.ParseFiles(files...)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = tmpl.Execute(w, nil)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	r.ParseForm()
+	// This is my own attempt to implement something like gorilla.schema
+	// It was good to use it to learn more about reflect package
+	// but probably I should use the existing gorilla.schema
+	v := Visitor{}
+	vt := reflect.TypeOf(v)
+	vp := reflect.ValueOf(&v)
+	vpe := vp.Elem()
+	for i := 0; i < vt.NumField(); i++ {
+		if vpe.Field(i).Kind() == reflect.String {
+			vpe.Field(i).SetString(r.PostForm[vt.Field(i).Tag.Get("form")][0])
+		}
+		if vpe.Field(i).Kind() == reflect.Bool {
+			if len(r.PostForm[vt.Field(i).Tag.Get("form")]) == 1 {
+				vpe.Field(i).SetBool(true)
+			} else {
+				vpe.Field(i).SetBool(false)
+			}
+		}
+	}
+	fmt.Fprintln(w, v)
+
+}
 
 func main() {
-	us, err := models.NewUserService(dbfile)
-	if err != nil {
-		panic(err)
-	}
-	us.AutoMigrate()
+	fs := http.FileServer(http.Dir(staticDir))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	staticDir := "/static"
-
-	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers(us)
-	listC := controllers.ListUsers(us)
-
-	r := mux.NewRouter()
-	r.Handle("/", staticC.Home).Methods("GET")
-	r.Handle("/contact", staticC.Contact).Methods("GET")
-	r.Handle("/confirmation", staticC.Confirmation).Methods("GET")
-	r.HandleFunc("/new", usersC.New).Methods("GET")
-	r.HandleFunc("/new", usersC.Create).Methods("POST")
-	r.HandleFunc("/list", listC.List).Methods("GET")
-	r.PathPrefix(staticDir).
-		Handler(http.StripPrefix(staticDir, http.FileServer(http.Dir("."+staticDir))))
-	http.ListenAndServe(":3000", r)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/new", newHandler)
+	http.HandleFunc("/confirmation", confirmHandler)
+	http.ListenAndServe(":3000", nil)
 }
