0951392a89af74c1fa0b11dab3b8bfd01e66eff1
Pavel Anni <pavel.anni@gmail.com>
2024-06-12 16:44:38 -0400

    add CreateAt field to Visitor

--- visitorstore/visitorStore.go
+++ visitorstore/visitorStore.go
@@ -2,21 +2,23 @@ package visitorstore
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/asdine/storm"
 )
 
 // Visitor contains information about Field Day visitors
 type Visitor struct {
-	ID        int    `storm:"id,increment"`
-	FirstName string `schema:"firstname"`
-	LastName  string `schema:"lastname"`
-	Callsign  string `schema:"callsign"`
-	Email     string `schema:"email"`
-	Nfarl     bool   `schema:"nfarl"`
-	Contactme bool   `schema:"contactme"`
-	Youth     bool   `schema:"youth"`
-	Firsttime bool   `schema:"firsttime"`
+	ID        int       `storm:"id,increment"`
+	CreatedAt time.Time `storm:"index"`
+	FirstName string    `schema:"firstname"`
+	LastName  string    `schema:"lastname"`
+	Callsign  string    `schema:"callsign"`
+	Email     string    `schema:"email"`
+	Nfarl     bool      `schema:"nfarl"`
+	Contactme bool      `schema:"contactme"`
+	Youth     bool      `schema:"youth"`
+	Firsttime bool      `schema:"firsttime"`
 }
 
 type VisitorStore struct {
@@ -38,6 +40,9 @@ func NewVisitorStore(dbFile string) (*VisitorStore, error) {
 func (vs *VisitorStore) SaveVisitor(v Visitor) error {
 	if v.FirstName == "" {
 		return fmt.Errorf("first name cannot be empty")
+	}
+	if v.CreatedAt.IsZero() {
+		v.CreatedAt = time.Now()
 	}
 	return vs.db.Save(&v)
 }
